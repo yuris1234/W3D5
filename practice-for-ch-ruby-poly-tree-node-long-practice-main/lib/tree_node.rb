79362dd51786e4c7fb6ff01f89106356a703c67e
yuris <yls4@cornell.edu>
2023-09-08 13:59:43 -0400

    Finished up to dfs

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -5,17 +5,36 @@ class PolyTreeNode
         @parent=nil
         @children=[]
     end
+
     def parent=(node)
         oldparent=parent
         if oldparent!=nil
             i= oldparent.children.index(self) 
-            p oldparent.children
             oldparent.children.delete_at(i)
-            p oldparent.children
         end
         @parent=node
     
         parent.children<<self if parent!=nil
     end
-  
+
+    def add_child(node)
+        node.parent=(self)
+    end
+
+    def remove_child(node)
+        raise if node.parent == nil
+        node.parent=nil
+    end
+
+    def dfs(target)
+        #base case
+        return self if value == target
+
+        children.each do |child|
+            child_value = child.dfs(target)
+            return child_value if child_value == target 
+        end
+
+        nil
+    end
 end
