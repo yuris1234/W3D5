cf060235b658f20b65a9761f20501be8b2d94098
yuris <yls4@cornell.edu>
2023-09-08 15:23:27 -0400

    finished initialize of knights class

---
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -0,0 +1,17 @@
+class KnightPathFinder
+    attr_reader :pos, :considered_positions, :initial_pos
+
+    def self.valid_moves(pos)
+
+    end
+
+    def initialize(pos)
+        @pos = pos
+        @considered_positions = [pos]
+        @initial_pos = self.root_node
+    end
+
+    def new_move_positions(pos)
+
+    end
+end
