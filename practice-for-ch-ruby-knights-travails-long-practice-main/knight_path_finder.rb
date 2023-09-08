a43c154203a0c05a8713be812ae34675f89f6698
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-08 12:37:29 -0700

    valid_pos

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -2,7 +2,23 @@ class KnightPathFinder
     attr_reader :pos, :considered_positions, :initial_pos
 
     def self.valid_moves(pos)
+        result=[]
+        valid =[[1,2],[2,1],[-2,1],[1,-2],[-2,-1],[-1,-2],[2,-1],[-1,2]]
+        valid.each do |sub|
+            result<<self.new_pos(pos, sub) if self.valid_pos(pos,sub)
+        end
+        result
+    end
 
+    def self.valid_pos(pos,move)
+        pos = self.new_pos(pos,move)
+        return true if (0..7).include?(pos[0]) && (0..7).include?(pos[1]) && !considered_positions?include(pos)
+        false
+    end
+    def self.new_pos(pos, move)
+        pos[0]=pos[0]+move[0]
+        pos[1]=pos[1]+move[1]
+        return pos 
     end
 
     def initialize(pos)
