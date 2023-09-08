df0a74213fd80fc619506c7af94c6522d8eb55a3
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-08 10:43:06 -0700

    finished parent

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    attr_reader :value,:parent,:children
+    def initialize(param)
+        @value=param
+        @parent=nil
+        @children=[]
+    end
+    def parent=(node)
+        oldparent=parent
+        if oldparent!=nil
+            i= oldparent.children.index(self) 
+            p oldparent.children
+            oldparent.children.delete_at(i)
+            p oldparent.children
+        end
+        @parent=node
+    
+        parent.children<<self if parent!=nil
+    end
   
 end
