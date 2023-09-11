7e0bfe81a9cb3c606c30d8e97839d6f9ced5c74d
yuris <yls4@cornell.edu>
2023-09-11 14:17:02 -0400

    Completed phase 1, 2, and 3 of knight path finder

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -2,7 +2,7 @@ require "byebug"
 require_relative "./poly_tree_node.rb"
 
 class KnightPathFinder
-    attr_reader :pos, :considered_positions, :initial_pos
+    attr_reader :start_pos, :considered_positions, :root_node
 
     def self.valid_moves(pos)
         result=[]
@@ -55,6 +55,25 @@ class KnightPathFinder
         end
     end
 
+    def trace_path_back(end_pos)
+        new_arr = []
+        new_arr << find_path_dfs(end_pos)
+        until new_arr[0] == root_node
+            new_arr.unshift(new_arr[0].parent)
+        end
+        new_arr
+    end
+
+    def find_path_bfs(end_pos)
+        root_node.bfs(end_pos)
+    end
+
+    def find_path_dfs(end_pos)
+        root_node.dfs(end_pos)
+    end
+
+
+
 end
 
 k = KnightPathFinder.new([0,0])
@@ -62,3 +81,5 @@ k = KnightPathFinder.new([0,0])
 # p KnightPathFinder.valid_moves([7,7])
 # p k.new_move_positions([-1,7])
 k.build_move_tree
+# p k.find_path([3,3])
+p k.trace_path_back([7,6])
